f372702bc4cc3dda4112d2447b242ae89a9758bd
tomoyu <tomoyuki719245@gmail.com>
2024-06-16 15:12:05 +0900

    init riverpod

--- lib/features/workouts/list_page.dart
+++ lib/features/workouts/list_page.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:my_strength_log_flutter/model/mock_workouts.dart';
 
-class ListPage extends StatelessWidget {
+final workoutsProvider = FutureProvider((ref) async {
+  await Future<void>.delayed(const Duration(seconds: 2));
+  return workoutsMocks;
+});
+
+class ListPage extends ConsumerWidget {
   const ListPage({super.key});
 
   static const icon = Icons.list;
   static const label = 'List';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('List Page'),
@@ -19,17 +26,35 @@ class ListPage extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: 100,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Item $index'),
-            onTap: () {
-              context.go('/list/detail');
-            },
-          );
-        },
-      ),
+      body: ref.watch(workoutsProvider).when(
+            data: (workouts) => RefreshIndicator(
+              onRefresh: () => ref.refresh(workoutsProvider.future),
+              child: ListView.builder(
+                itemCount: workouts.length,
+                itemBuilder: (context, index) {
+                  final workout = workouts[index];
+                  return ListTile(
+                    title: Text(workout.id),
+                    onTap: () =>
+                        context.push('/list/detail', extra: workout.id),
+                  );
+                },
+              ),
+            ),
+            loading: () => const Center(child: CircularProgressIndicator()),
+            error: (error, stackTrace) => Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Error occurred'),
+                  ElevatedButton(
+                    onPressed: () => ref.refresh(workoutsProvider),
+                    child: const Text('Retry'),
+                  ),
+                ],
+              ),
+            ),
+          ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_strength_log_flutter/app.dart';
 
-void main() => runApp(const App());
+void main() => runApp(const ProviderScope(child: App()));
